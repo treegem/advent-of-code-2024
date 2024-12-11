f0ef05ac994015061d051ca90926e10dd40a9b0c
gbrown <georg.braunbeck@googlemail.com>
2024-12-11 15:58:08 +0100

    day 11 part 1

--- src/main/kotlin/de/gbrown/aoc2024/Day11.kt
+++ src/main/kotlin/de/gbrown/aoc2024/Day11.kt
@@ -7,9 +7,45 @@ import de.gbrown.aoc2024.util.solve
 
 object Day11 {
 
-    fun part1(input: List<String>): Int = TODO("Must process $input")
+    fun part1(input: List<String>): Int {
+        var stones = input.first().split(" ").map { it.toLong() }
 
-    fun part2(input: List<String>): Int = TODO("Must process $input")
+        repeat(25) { stones = blink(stones) }
+
+        return stones.count()
+    }
+
+    fun part2(input: List<String>): Int {
+        var stones = input.first().split(" ").map { it.toLong() }
+
+        repeat(75) { counter ->
+            if (counter % 5 == 0) {
+                println(counter)
+            }
+            stones = blink(stones)
+        }
+
+        return stones.count()
+    }
+
+    private fun addSplitStones(newStones: MutableList<Long>, stone: Long) {
+        val stoneString = stone.toString()
+        val half = stoneString.length / 2
+        newStones.add(stoneString.substring(startIndex = 0, endIndex = half).toLong())
+        newStones.add(stoneString.substring(startIndex = half).toLong())
+    }
+
+    private fun blink(stones: List<Long>): List<Long> {
+        val newStones = mutableListOf<Long>()
+        stones.forEach { stone ->
+            when {
+                stone == 0L -> newStones.add(1)
+                (stone.toString().length % 2) == 0 -> addSplitStones(newStones, stone)
+                else -> newStones.add(stone * 2024)
+            }
+        }
+        return newStones
+    }
 }
 
 fun main() {
