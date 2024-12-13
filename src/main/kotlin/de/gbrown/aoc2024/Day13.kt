2a3b24b26c37a0fa8f8438ca6202505ffd22330c
gbrown <georg.braunbeck@googlemail.com>
2024-12-13 09:30:16 +0100

    day 13 setup

---
+++ src/main/kotlin/de/gbrown/aoc2024/Day13.kt
@@ -0,0 +1,26 @@
+@file:Suppress("MagicNumber")
+
+package de.gbrown.aoc2024
+
+import de.gbrown.aoc2024.util.checkOnTestInput
+import de.gbrown.aoc2024.util.solve
+
+object Day13 {
+
+    fun part1(input: List<String>): Int = TODO("Must process $input")
+
+    fun part2(input: List<String>): Int = TODO("Must process $input")
+}
+
+fun main() {
+
+    val day = 13
+
+    println("\nPart 1:")
+    checkOnTestInput(day, 0, Day13::part1)
+    solve(day, Day13::part1)
+
+    println("\nPart2:")
+    checkOnTestInput(day, 0, Day13::part2)
+    solve(day, Day13::part2)
+}
