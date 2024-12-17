e083325daeef4736a8d1e5130d1d491276670bf1
gbrown <georg.braunbeck@googlemail.com>
2024-12-17 11:33:23 +0100

    day 16 part 2

--- src/main/kotlin/de/gbrown/aoc2024/Day16.kt
+++ src/main/kotlin/de/gbrown/aoc2024/Day16.kt
@@ -8,6 +8,7 @@ import de.gbrown.aoc2024.util.checkOnTestInput
 import de.gbrown.aoc2024.util.findAllPositionsByPredicate
 import de.gbrown.aoc2024.util.solve
 import de.gbrown.aoc2024.util.to2dCharsListMatrix
+import java.util.PriorityQueue
 
 object Day16 {
 
@@ -16,15 +17,10 @@ object Day16 {
 
 
     fun part1(input: List<String>): Long {
-        val map2d = input.to2dCharsListMatrix()
-        val startingPosition = map2d.findAllPositionsByPredicate { it == START }.single()
-        val endPosition = map2d.findAllPositionsByPredicate { it == END }.single()
-        val allOtherNodePosition = map2d.findAllPositionsByPredicate { it == '.' }
+        val (startingPosition, endPosition, allOtherNodePosition) = findAllNodePositions(input)
 
         val startingNode = Node(startingPosition, Direction.RIGHT)
-        val allNodesExceptStart = (allOtherNodePosition + endPosition).map { position ->
-            Direction.nonDiagonals.map { Node(position, it) }
-        }.flatten()
+        val allNodesExceptStart = createAllNodesExceptStart(allOtherNodePosition, endPosition)
 
         val distances = allNodesExceptStart.associateWith { Long.MAX_VALUE }.toMutableMap()
         distances[startingNode] = 0L
@@ -32,28 +28,118 @@ object Day16 {
         return shortestPath((allNodesExceptStart + startingNode), endPosition, distances)
     }
 
-    fun part2(input: List<String>): Long = TODO("Must process $input")
+    fun part2(input: List<String>): Long {
+        val (startingPosition, endPosition, allOtherNodePosition) = findAllNodePositions(input)
+
+        val startingNode = Node(startingPosition, Direction.RIGHT)
+        val allNodesExceptStart = createAllNodesExceptStart(allOtherNodePosition, endPosition)
+
+        val distances = allNodesExceptStart.associateWith { Long.MAX_VALUE }.toMutableMap()
+        distances[startingNode] = 0L
+
+        val shortestPaths = findAllShortestPaths((allNodesExceptStart + startingNode), endPosition, distances)
+        return shortestPaths.map { it.position }.toSet().size.toLong()
+    }
+
+    private fun createAllNodesExceptStart(
+        allOtherNodePosition: List<Position>,
+        endPosition: Position,
+    ) = (allOtherNodePosition + endPosition).map { position ->
+        Direction.nonDiagonals.map { Node(position, it) }
+    }.flatten()
 
     private fun shortestPath(
         nodes: List<Node>,
         endPosition: Position,
         distances: MutableMap<Node, Long>,
     ): Long {
-        val unvisitedNodes = nodes.toMutableSet()
+        val unvisitedNodes = PriorityQueue(compareBy<Node> { distances[it]!! })
+        unvisitedNodes.addAll(nodes)
 
         while (unvisitedNodes.isNotEmpty()) {
-            val currentNode = getNodeWithShortestDistance(unvisitedNodes, distances).also { unvisitedNodes.remove(it) }
+            val currentNode = unvisitedNodes.poll()
 
             findAllRelevantNeighbors(currentNode, nodes).forEach { nextNode ->
                 if (nextNode in unvisitedNodes) {
+                    val oldDistance = distances[nextNode]!!
                     updateDistance(currentNode, nextNode, distances)
+                    if (distances[nextNode]!! < oldDistance) {
+                        unvisitedNodes.remove(nextNode)
+                        unvisitedNodes.add(nextNode)
+                    }
                 }
             }
-            if (unvisitedNodes.none { it.position == endPosition }) {
-                return distances.filter { it.key.position == endPosition }.values.minOrNull()!!
+            if (currentNode.position == endPosition) {
+                return distances[currentNode]!!
             }
         }
         return -1
+    }
+
+    private fun findAllShortestPaths(
+        nodes: List<Node>,
+        endPosition: Position,
+        distances: MutableMap<Node, Long>,
+    ): List<Node> {
+        val unvisitedNodes = PriorityQueue(compareBy<Node> { distances[it]!! })
+        unvisitedNodes.addAll(nodes)
+        val predecessors: MutableMap<Node, List<Node>> = mutableMapOf()
+
+        while (unvisitedNodes.isNotEmpty()) {
+            val currentNode = unvisitedNodes.poll()
+
+            findAllRelevantNeighbors(currentNode, nodes).forEach { nextNode ->
+                if (nextNode in unvisitedNodes) {
+                    val oldDistance = distances[nextNode]!!
+                    updateDistanceAndPredecessors(currentNode, nextNode, distances, predecessors)
+                    if (distances[nextNode]!! < oldDistance) {
+                        unvisitedNodes.remove(nextNode)
+                        unvisitedNodes.add(nextNode)
+                    }
+                }
+            }
+            if (currentNode.position == endPosition) {
+                return extractPaths(predecessors, currentNode)
+            }
+        }
+        error("Algorithm failed")
+    }
+
+    private fun extractPaths(
+        predecessors: MutableMap<Node, List<Node>>,
+        endNode: Node,
+    ): List<Node> {
+        val path = mutableListOf(endNode)
+        addAllPredecessorsToPath(endNode, predecessors, path)
+        return path
+    }
+
+    private fun addAllPredecessorsToPath(
+        currentNode: Node,
+        predecessors: MutableMap<Node, List<Node>>,
+        path: MutableList<Node>,
+    ) {
+        val currentPredecessors = predecessors[currentNode]
+        if (currentPredecessors != null) {
+            path.addAll(currentPredecessors)
+            currentPredecessors.forEach { addAllPredecessorsToPath(it, predecessors, path) }
+        }
+    }
+
+    private fun updateDistanceAndPredecessors(
+        currentNode: Node,
+        nextNode: Node,
+        distances: MutableMap<Node, Long>,
+        predecessors: MutableMap<Node, List<Node>>,
+    ) {
+        val distanceToNextNode = distances[currentNode]!! + distanceBetween(currentNode, nextNode)
+        if (distanceToNextNode == distances[nextNode]) {
+            predecessors[nextNode] = predecessors[nextNode]!! + currentNode
+        }
+        if (distanceToNextNode < distances[nextNode]!!) {
+            distances[nextNode] = distanceToNextNode
+            predecessors[nextNode] = listOf(currentNode)
+        }
     }
 
     private fun updateDistance(
@@ -79,10 +165,14 @@ object Day16 {
         return distance
     }
 
-    private fun getNodeWithShortestDistance(
-        unvisitedNodes: MutableSet<Node>,
-        distances: MutableMap<Node, Long>,
-    ) = unvisitedNodes.minByOrNull { distances[it]!! }!!
+    private fun findAllNodePositions(input: List<String>): Triple<Position, Position, List<Position>> {
+        val map2d = input.to2dCharsListMatrix()
+        val startingPosition = map2d.findAllPositionsByPredicate { it == START }.single()
+        val endPosition = map2d.findAllPositionsByPredicate { it == END }.single()
+        val allOtherNodePosition = map2d.findAllPositionsByPredicate { it == '.' }
+
+        return Triple(startingPosition, endPosition, allOtherNodePosition)
+    }
 }
 
 private fun findAllRelevantNeighbors(centerNode: Node, nodes: List<Node>): List<Node> =
@@ -104,6 +194,7 @@ fun main() {
     solve(day, Day16::part1)
 
     println("\nPart2:")
-    checkOnTestInput(day, 0L, Day16::part2)
+    checkOnTestInput(day, 45L, Day16::part2, suffix = "_ex1")
+    checkOnTestInput(day, 64L, Day16::part2, suffix = "_ex2")
     solve(day, Day16::part2)
 }
--- src/main/kotlin/de/gbrown/aoc2024/util/Coordinate2D.kt
+++ src/main/kotlin/de/gbrown/aoc2024/util/Coordinate2D.kt
@@ -1,6 +1,8 @@
 package de.gbrown.aoc2024.util
 
-abstract class Coordinate2D(open val x: Int, open val y: Int) {
+abstract class Coordinate2D(open val x: Int, open val y: Int)
+
+data class Position(override val x: Int, override val y: Int) : Coordinate2D(x, y) {
 
     fun moved(direction: Direction, steps: Int = 1): Position = when (direction) {
         Direction.UP -> Position(x, y - steps)
@@ -13,18 +15,15 @@ abstract class Coordinate2D(open val x: Int, open val y: Int) {
         Direction.UP_LEFT -> Position(x - steps, y - steps)
     }
 
-    fun moved(vector: Coordinate2D, steps: Int = 1): Position = Position(x + vector.x * steps, y + vector.y * steps)
+    fun moved(vector: Position, steps: Int = 1): Position = Position(x + vector.x * steps, y + vector.y * steps)
+
+    fun isWithinBounds(xs: IntRange, ys: IntRange) = x in xs && y in ys
 
     operator fun plus(other: Position) = Position(x + other.x, y + other.y)
 
     operator fun minus(other: Position) = Position(x - other.x, y - other.y)
+}
 
+data class Velocity(override val x: Int, override val y: Int) : Coordinate2D(x, y) {
     operator fun times(scalar: Int) = Position(x * scalar, y * scalar)
 }
-
-data class Position(override val x: Int, override val y: Int) : Coordinate2D(x, y) {
-
-    fun isWithinBounds(xs: IntRange, ys: IntRange) = x in xs && y in ys
-}
-
-data class Velocity(override val x: Int, override val y: Int) : Coordinate2D(x, y)
