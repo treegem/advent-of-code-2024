897ed8cd8d37fdde0a0293268906ac2e72b0f40e
gbrown <georg.braunbeck@googlemail.com>
2024-12-20 11:32:25 +0100

    day 19 part 2 fast enough

--- src/main/kotlin/de/gbrown/aoc2024/Day19.kt
+++ src/main/kotlin/de/gbrown/aoc2024/Day19.kt
@@ -13,12 +13,10 @@ object Day19 {
         return designs.count { canBeArranged(it, towels) }
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val (towels: List<String>, designs: List<String>) = extractTowelsAndDesigns(input)
-
-        return designs
-            .filter { canBeArranged(it, towels) }
-            .sumOf { countArrangements(it, towels) }
+        val arrangementCache = mutableMapOf<String, Long>()
+        return designs.sumOf { countArrangements(it, towels, arrangementCache) }
     }
 
     private fun extractTowelsAndDesigns(input: List<String>): Pair<List<String>, List<String>> {
@@ -47,19 +45,21 @@ object Day19 {
     private fun countArrangements(
         design: String,
         towels: List<String>,
-    ): Int {
-        val arrangements = mutableMapOf<String, MutableSet<List<String>>>().withDefault { mutableSetOf() }
-        towels.forEach { arrangements[it] = mutableSetOf(listOf(it)) }
-
-        var updated = -1
-        println("design: $design")
-
-        while (updated != 0) {
-            updated = updateArrangements(arrangements, design)
+        arrangementCache: MutableMap<String, Long>,
+    ): Long =
+        when {
+            design.isEmpty() -> 1
+            design in arrangementCache -> arrangementCache[design]!!
+            else -> {
+                towels.sumOf { towel ->
+                    if (design.startsWith(towel)) {
+                        countArrangements(design.removePrefix(towel), towels, arrangementCache)
+                    } else {
+                        0
+                    }
+                }.also { arrangementCache[design] = it }
+            }
         }
-        arrangements.filterKeys { design.startsWith(it) }.maxBy { it.key.length }.also { println(it) }
-        return arrangements.getValue(design).size
-    }
 
     private fun calculateNextAttempts(
         constructionAttempts: MutableList<String>,
@@ -76,48 +76,18 @@ object Day19 {
         }
         return nextAttempts.toList()
     }
-
-    private fun updateArrangements(
-        arrangements: MutableMap<String, MutableSet<List<String>>>,
-        targetDesign: String,
-    ): Int {
-        var updated = 0
-        val keys = arrangements.keys
-        val newArrangements = mutableMapOf<String, MutableSet<List<String>>>()
-        keys.forEach { root ->
-            keys.forEach inner@{ newBuildingBlock ->
-                val combinedKeys = root + newBuildingBlock
-                if (targetDesign.startsWith(combinedKeys)) {
-                    arrangements.getValue(root).flatMap { rootArrangements ->
-                        arrangements.getValue(newBuildingBlock).map { newBlockArrangements ->
-                            val combinedBlocks = rootArrangements + newBlockArrangements
-                            if (combinedBlocks !in arrangements.getValue(combinedKeys)) {
-                                newArrangements[combinedKeys] =
-                                    arrangements.getValue(combinedKeys).apply { add(combinedBlocks) }
-                                updated++
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        newArrangements.forEach { (key, value) ->
-            arrangements[key] = (arrangements.getValue(key) + value).toMutableSet()
-        }
-        return updated
-    }
 }
 
 fun main() {
 
     val day = 19
 
-//    println("\nPart 1:")
-//    checkOnTestInput(day, 6, Day19::part1)
-//    solve(day, Day19::part1)
+    println("\nPart 1:")
+    checkOnTestInput(day, 6, Day19::part1)
+    solve(day, Day19::part1)
 
     println("\nPart2:")
-    checkOnTestInput(day, 7, Day19::part2, suffix = "_ex1")
-    checkOnTestInput(day, 16, Day19::part2)
+    checkOnTestInput(day, 7L, Day19::part2, suffix = "_ex1")
+    checkOnTestInput(day, 16L, Day19::part2)
     solve(day, Day19::part2)
 }
